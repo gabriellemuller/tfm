537e061cb66cb9cae92ec8957e22627a9989ff2c
gabriellemuller <g.muller@alumnos.upm.es>
2025-08-06 16:02:11 +0200

    Create DP_Colaboración.py

---
+++ DP_Colaboración.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  4 15:18:06 2025
+
+@author: gabri
+"""
+
+import random
+import matplotlib.pyplot as plt
+
+#parameters
+R = 1     
+S = 0     
+T = 1.5   
+P = 0    
+N = 10    
+ROUNDS = 10
+
+#aleatory initiation, c=cooperation, d=defection
+agents = {i: random.choice(['C', 'D']) for i in range(N)}
+cooperation = []
+
+#DP's simulation
+def play_game(s1, s2):
+    if s1 == 'C' and s2 == 'C':
+        return R, R
+    elif s1 == 'C' and s2 == 'D':
+        return S, T
+    elif s1 == 'D' and s2 == 'C':
+        return T, S
+    else:
+        return P, P
+
+#simulation 
+for _ in range(ROUNDS):
+    #random peers creation
+    players = list(agents.keys())
+    random.shuffle(players)
+    peers = [(players[i], players[i+1]) for i in range(0, N, 2)]
+
+    #payoffs
+    payoffs = {i: 0 for i in agents}
+    for a, b in peers:
+        pa, pb = play_game(agents[a], agents[b])
+        payoffs[a] += pa
+        payoffs[b] += pb
+
+    #strategy update
+    new_agents = agents.copy()
+    for i in agents:
+        j = random.choice([k for k in agents if k != i])
+        if payoffs[j] > payoffs[i]:
+            diff = payoffs[j] - payoffs[i]
+            delta_max = max(1, T) - min(0, S)
+            prob = diff / delta_max
+            if random.random() < prob:
+                new_agents[i] = agents[j]
+    agents = new_agents
+
+    #cooperation rate
+    coop_ratio = sum(1 for s in agents.values() if s == 'C') / N
+    cooperation.append(coop_ratio)
+
+# Affichage des résultats
+plt.plot(range(1, ROUNDS + 1), cooperation, marker='o', color='green')
+plt.xlabel("Ronda")
+plt.ylabel("Cooperación media")
+plt.title("Evolución de la cooperación entre 10 agentes")
+plt.grid(True)
+plt.show()
