4c59fdbfbb758f59f9972fef0aac192400f8a8a4
gabriellemuller <g.muller@alumnos.upm.es>
2025-08-07 11:40:33 +0200

    Update DP_Colaboración.py

--- DP_Colaboración.py
+++ DP_Colaboración.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Aug  4 15:18:06 2025
 
-@author: gabri
+@author: Gabrielle Muller
 """
 
 import random
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 #parameters
 R = 1     
 S = 0     
-T = 1.5   
+T = 2 
 P = 0    
 N = 10    
-ROUNDS = 10
+rounds = 10
 
 #aleatory initiation, c=cooperation, d=defection
 agents = {i: random.choice(['C', 'D']) for i in range(N)}
@@ -32,7 +32,7 @@ def play_game(s1, s2):
         return P, P
 
 #simulation 
-for _ in range(ROUNDS):
+for k in range(rounds):
     #random peers creation
     players = list(agents.keys())
     random.shuffle(players)
@@ -51,8 +51,8 @@ for _ in range(ROUNDS):
         j = random.choice([k for k in agents if k != i])
         if payoffs[j] > payoffs[i]:
             diff = payoffs[j] - payoffs[i]
-            delta_max = max(1, T) - min(0, S)
-            prob = diff / delta_max
+            phi = (N-1)*(max(1, T) - min(0, S))
+            prob = diff / phi
             if random.random() < prob:
                 new_agents[i] = agents[j]
     agents = new_agents
@@ -62,7 +62,9 @@ for _ in range(ROUNDS):
     cooperation.append(coop_ratio)
 
 # Affichage des résultats
-plt.plot(range(1, ROUNDS + 1), cooperation, marker='o', color='green')
+for i, c in enumerate(cooperation, 1):
+    print(f"Ronda {i} : Cooperación media = {c:.2f}")
+plt.plot(range(1, rounds + 1), cooperation, marker='o', color='green')
 plt.xlabel("Ronda")
 plt.ylabel("Cooperación media")
 plt.title("Evolución de la cooperación entre 10 agentes")
